eedb1eb47ef1628021f9b63e987416651fe981a5
Ancci1617 <92834293+Ancci1617@users.noreply.github.com>
2023-01-19 14:54:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -87,8 +87,8 @@ app.post('/save-subscription', async (req, res) => {
 })
 
 //Server
-app.listen(3000, () => {
-    console.log("Running on port", 3000)
+app.listen(process.env.PORT||3000 , () => {
+    console.log("Running on port", process.env.PORT||3000)
 })
 
 
